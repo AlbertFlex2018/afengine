faaf0fe78532332c81a5bc35214ca0b8b74cc5df
Albert Flex <2662842460@qq.com>
2020-01-03 13:37:19 +0800

    add ui controls.

--- src/afengine/part/uiinput/control/UIToggle.java
+++ src/afengine/part/uiinput/control/UIToggle.java
@@ -2,8 +2,13 @@ package afengine.part.uiinput.control;
 
 import afengine.core.util.Debug;
 import afengine.core.util.Vector;
+import afengine.core.window.IGraphicsTech;
 import afengine.core.window.ITexture;
+import afengine.part.message.Message;
+import afengine.part.uiinput.InputServlet;
 import afengine.part.uiinput.UIActor;
+import afengine.part.uiinput.UIFace;
+import java.awt.event.MouseEvent;
 
 /**
  *
@@ -40,17 +45,17 @@ public class UIToggle extends UIActor{
     public int getCapacity() {
         return capacity;
     }
-    public void pushToggle(ITexture texture,IUIAction action){
+    public void pushToggle(ITexture texture,IUIAction toToggleaction){
         if(addindex>(capacity-1)){
             Debug.log("it's full on toggles,please do not add more.");
             return;
         }
-        if(texture==null||action==null){
+        if(texture==null||toToggleaction==null){
             Debug.log("texture or action should not null.");
             return;
         }
         textures[addindex]=texture;
-        actions[addindex]=action;
+        actions[addindex]=toToggleaction;
         ++addindex;
     }
     public void popToggle(){
@@ -60,5 +65,63 @@ public class UIToggle extends UIActor{
         }
         --addindex;
     }
+    public IUIAction getToggleActionAt(int eindex){
+        if(eindex<0||eindex>=addindex)return null;
+        
+        return actions[eindex];
+    }
+    public ITexture getToggleTextureAt(int eindex){
+        if(eindex<0||eindex>=addindex)return null;
+        
+        return textures[eindex];        
+    }
+    @Override
+    public void update(long time){
+        ITexture text=textures[index];
+        if(text!=null){
+            super.width=text.getWidth();
+            super.height=text.getHeight();
+        }
+    }
     
+    //INPUT_MOUSE_CLICK
+    @Override
+    public boolean handle(Message msg){
+        MouseEvent event=(MouseEvent) msg.extraObjs[0];
+        if(event!=null){
+            int mx=event.getX();
+            int my=event.getY();
+            boolean in=super.isPointInUi(mx, my);
+            if(in){
+                toggle();
+                IUIAction action=actions[index];
+                action.action(this);
+                return true;
+            }
+        }
+        return false;
+    }
+    public void toggle(){
+        index=(index+1)%(addindex+1);
+    }
+
+    @Override
+    public void draw(IGraphicsTech tech){
+        ITexture text=textures[index];
+        if(text!=null){
+            int dx=super.getUiX();
+            int dy=super.getUiY();
+            tech.drawTexture(dx,dy,text);
+        }
+    }   
+
+    @Override
+    protected void loadOutFromFace(UIFace face) {
+        face.removeMsgUiMap(InputServlet.INPUT_MOUSE_CLICK,this);
+    }
+
+    @Override
+    protected void loadInToFace(UIFace face) {
+        face.addMsgUiMap(InputServlet.INPUT_MOUSE_CLICK,this);
+    }    
 }
