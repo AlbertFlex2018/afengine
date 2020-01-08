eb79c3c50c8e170e4f1838915f50775b2ac59c4b
Albert Flex <2662842460@qq.com>
2020-01-08 14:32:51 +0800

    add uifilehelp.

--- src/afengine/part/uiinput/control/UITextButton.java
+++ src/afengine/part/uiinput/control/UITextButton.java
@@ -1,61 +1,13 @@
 package afengine.part.uiinput.control;
 
-import afengine.core.AppState;
-import afengine.core.WindowApp;
 import afengine.core.util.TextCenter.Text;
 import afengine.core.util.Vector;
 import afengine.core.window.IColor;
 import afengine.core.window.IFont;
 import afengine.core.window.IGraphicsTech;
-import afengine.part.uiinput.UIActor;
-import org.dom4j.Element;
 
 
-public class UITextButton extends UIButtonBase{
-    
-    public static class UITextButtonCreator implements IUIActorCreator{
-        
-        /*
-            <UITextButton reserve="" name="" pos="">
-                <Text></Text>
-                <Font></Font>
-                <Color></Color>
-                <Actions>
-                    <toCover class=""/>
-                    <toNormal class=""/>
-                    <toDown class=""/>
-                </Actions>
-            </UITextButton>
-        */
-        @Override
-        public UIActor createUi(Element element){
-            String name=element.attributeValue("name");
-            String pos=element.attributeValue("pos");
-            Element text=element.element("Text");
-            Element font=element.element("Font");
-            Element color=element.element("Color");
-            Element actions=element.element("Actions");            
-
-            Vector posv=new Vector(0,0,0);
-            String[] poss=pos.split(",");
-            if(poss!=null){
-                double x=Double.parseDouble(poss[0]);
-                double y=Double.parseDouble(poss[1]);                
-                posv.setX(x);
-                posv.setY(y);
-            }
-            
-            Text textt=new Text("DefaultText");
-            if(text!=null){
-                textt.value=text.getText();
-            }
-            
-            IGraphicsTech tech=((WindowApp)(AppState.getRunningApp())).getGraphicsTech();
-            
-            return null;
-        }        
-    }
-    
+public class UITextButton extends UIButtonBase{        
     private Text text;
     private IFont font;
     private IColor fontColor;    
